fdab90b58af4bfcc9f9e9eb83a3006b1039fbcb3
Sayeed <sayeed.prof1@gmaiil.com>
2026-04-15 02:24:56 -0600

    Added core RM models

--- core/models.py
+++ core/models.py
@@ -1,17 +1,37 @@
 from django.db import models
 
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.FloatField()
-    stock = models.IntegerField()
 
-    def __str__(self):
-        return self.name
-    
 class Customer(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
+    customer_id = models.AutoField(primary_key=True)
+    customer_name = models.CharField(max_length=100)
+    contact_info = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.customer_name
+
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    product_description = models.TextField()
+    unit_price = models.FloatField()
+
+    def __str__(self):
+        return self.product_name
+
+
+class Transaction(models.Model):
+    transaction_id = models.AutoField(primary_key=True)
+    transaction_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"Transaction {self.transaction_id}"
+
+
+class TransactionLine(models.Model):
+    transaction_line_id = models.AutoField(primary_key=True)
+    quantity = models.IntegerField()
+    unit_price = models.FloatField()
+
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
