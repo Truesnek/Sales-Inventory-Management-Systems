b363e1326752ee595a111318b090d761ba62a999
bailey li <bailey.yao@ucalgary.ca>
2026-04-16 09:58:19 -0600

    added products and customer views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Products, Customer
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def product_list(request):
+    products = Products.objects.all()
+    return render(request, 'core/product_list.html', {'products': products})
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    return render(request, 'core/customer_list.html', {'customers': customers})
