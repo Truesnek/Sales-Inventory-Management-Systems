7d228c0c006277199887600feed7ab167df1cc9c
bailey li <bailey.yao@ucalgary.ca>
2026-04-16 13:31:37 -0600

    added an s to the end of generates report to fix html

--- sales_inventory_management/settings.py
+++ sales_inventory_management/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb',
         'USER': 'root',
-        'PASSWORD': 'Enter-password',
+        'PASSWORD': 'Catman123456789',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
