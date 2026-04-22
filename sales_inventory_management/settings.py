b84abaa28557c30122766798ad0b09bd4fb4eadd
Sayeed <sayeed.prof1@gmaiil.com>
2026-04-22 09:22:05 -0600

    coded branch_inventory.html

--- sales_inventory_management/settings.py
+++ sales_inventory_management/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb',
         'USER': 'root',
-        'PASSWORD': 'Enter-password',
+        'PASSWORD': 'root123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
